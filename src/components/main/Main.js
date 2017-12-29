abf6d4dc7a756b9ab7cf03cbf205fa7523248ef3
wilsonespina <wilson.espina@gmail.com>
2017-12-29 17:30:49 +0000

    AWS image

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,14 +6,13 @@ import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy,
 const Main = ({scrollTo}) => {
 
   return(
-    <Row className="main-section section container-fluid">
-      <img className="main-head-img" src="../../assets/tube_wallpaper.jpg"/>
+    <Row className="main-section section container-fluid"> <img className="main-head-img" src="../../assets/tube_wallpaper.jpg"/>
       <Element name="main" className="element" >
         <h1 className="heading">Main Section</h1>
       </Element>
 
 
-      <img src="https://user-images.githubusercontent.com/28314323/32404855-373aa592-c151-11e7-9661-5e870aca48b6.jpg" alt="profile-img" className="profile-pic responsive-img"/>
+      <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/headshots/HEADSHOT.png" alt="profile-img" className="profile-pic responsive-img"/>
 
       <h2>Wilson Espina</h2>
       <h3>Welcome to my Portfolio....</h3>
