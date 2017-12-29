55585dca9924de8f4fd45b346aa9574621e7ecfc
wilsonespina <wilson.espina@gmail.com>
2017-12-29 13:13:28 +0000

    sorting grid layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import SideNav, { MenuIcon } from 'react-simple-sidenav';
 
 import NavBar from './components/utility/NavBar';
--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
 import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 const Projects = ({scrollTo}) => {
 
   return(
-    <Row className="projects-section section container-fluid">
+    <Row className="projects-section section fluid">
 
       <Element name="projects" className="element" >
-        <h1 className="projects-heading">Projects Section</h1>
+        <h1 className="projects-heading">Projects</h1>
       </Element>
 
+      <Row className="project-4">
+        <Col xs={11} sm={11} smOffset={0.5} md={10} mdOffset={1} lg={10} lgOffset={1} className="project-tile">
+          <Col md={6}>
+            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-4/README/screenshot_landing.jpg" className="project-img"/>
+          </Col>
 
-      <a onClick={scrollTo}>To the top!</a>
+          <Col md={6}>
+            <h1>Project 4 - 'TRACE' </h1>
+            <h2>MERN stack application</h2>
+            <p>Prject at GA</p>
+          </Col>
+
+        </Col>
+      </Row>
+
+
+      {/* <a onClick={scrollTo}>To the top!</a> */}
     </Row>
   );
 };
