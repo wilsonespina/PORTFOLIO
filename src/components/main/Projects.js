19bec275b857ac47d6612118c4a274ba134da14b
wilsonespina <wilson.espina@gmail.com>
2017-12-29 14:43:34 +0000

    default font added

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Row, Col, Grid } from 'react-bootstrap';
+import { Row, Col, Carousel } from 'react-bootstrap';
 import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 const Projects = ({scrollTo}) => {
@@ -14,16 +14,58 @@ const Projects = ({scrollTo}) => {
 
       <Row className="project-4">
         <Col xs={11} sm={11} smOffset={0.5} md={10} mdOffset={1} lg={10} lgOffset={1} className="project-tile">
-          <Col md={6}>
-            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-4/README/screenshot_landing.jpg" className="project-img"/>
+          <Col md={6} className="project-image-left">
+            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-4/README/screenshot_landing.jpg" alt="image01" className="project-img"/>
+
           </Col>
 
-          <Col md={6}>
+          <Col md={6} className="project-text-right">
             <h1>Project 4 - 'TRACE' </h1>
             <h2>MERN stack application</h2>
-            <p>Prject at GA</p>
+            <p>Project at GA</p>
+          </Col>
+        </Col>
+      </Row>
+
+      <Row className="project-3">
+        <Col xs={11} sm={11} smOffset={0.5} md={10} mdOffset={1} lg={10} lgOffset={1} className="project-tile">
+          <Col md={6} className="project-image-left">
+            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-4/README/screenshot_landing.jpg" alt="image01" className="project-img"/>
           </Col>
 
+          <Col md={6} className="project-text-right">
+            <h1>Project 3 - 'Gigly' </h1>
+            <h2>Group Project</h2>
+            <p>Project at GA</p>
+          </Col>
+        </Col>
+      </Row>
+
+      <Row className="project-2">
+        <Col xs={11} sm={11} smOffset={0.5} md={10} mdOffset={1} lg={10} lgOffset={1} className="project-tile">
+          <Col md={6} className="project-image-left">
+            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-2/README/screenshot_landingpage.jpg" alt="image01" className="project-img"/>
+          </Col>
+
+          <Col md={6} className="project-text-right">
+            <h1>Project 2 - 'KatSuper Store' </h1>
+            <h2>RESTful app</h2>
+            <p>Project at GA</p>
+          </Col>
+        </Col>
+      </Row>
+
+      <Row className="project-1">
+        <Col xs={11} sm={11} smOffset={0.5} md={10} mdOffset={1} lg={10} lgOffset={1} className="project-tile">
+          <Col md={6} className="project-image-left">
+            <img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-1/README/screenshot_game.jpg" alt="image01" className="project-img"/>
+          </Col>
+
+          <Col md={6} className="project-text-right">
+            <h1>Project 1 - 'Can You Diglett?' </h1>
+            <h2>JavaScript Game</h2>
+            <p>Project at GA</p>
+          </Col>
         </Col>
       </Row>
 
