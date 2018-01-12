2ed43507ad30c2a35babb1db4f49109da706a6e7
wilsonespina <wilson.espina@gmail.com>
2018-01-12 17:20:30 +0000

    build command entered

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
-const env = process.env.NODE_ENV || 'development';
-const port  = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/portfolio-${env}`;
-const secret = process.env.AUTH_SECRET || 'hYgs^=?>@qrTfxLp';
-
-module.exports = { port, dbURI, secret, env };
+// const env = process.env.NODE_ENV || 'development';
+// const port  = process.env.PORT || 4000;
+// const dbURI = process.env.MONGODB_URI || `mongodb://localhost/portfolio-${env}`;
+// const secret = process.env.AUTH_SECRET || 'hYgs^=?>@qrTfxLp';
+//
+// module.exports = { port, dbURI, secret, env };
