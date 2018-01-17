dd9e0a2277d15a9d047ca74f578aa607b6901ea4
wilsonespina <wilson.espina@gmail.com>
2018-01-17 16:03:27 +0000

    contact section updated

--- src/components/utility/Contact.js
+++ src/components/utility/Contact.js
@@ -14,13 +14,13 @@ const Contact = ({scrollTo}) => {
               <h3 className="span-pink margin-bottom-50"><span className="large-font">Contact</span></h3>
 
               <div className="contact-links-tile large-font span-blue">
-                <a href="mailto:wilson.espina@gmail.com"><div className="email"><i className="fa fa-envelope" aria-hidden="true"></i>Email - <span className="span-white"><em>wilson.espina@gmail.com</em></span></div></a>
+                <a href="mailto:wilson.espina@gmail.com"><div className="email"><i className="fa fa-envelope" aria-hidden="true"></i><span className="disappear">Email - </span><span className="span-white"><em>wilson.espina@gmail.com</em></span></div></a>
 
-                <a href="https://github.com/wilsonespina" target="blank"><div className="github"><i className="fa fa-github" aria-hidden="true"></i>GitHub - <span className="span-white"><em>wilsonespina</em></span></div></a>
+                <a href="https://github.com/wilsonespina" target="blank"><div className="github"><i className="fa fa-github" aria-hidden="true"></i><span className="disappear">GitHub -</span> <span className="span-white"><em>wilsonespina</em></span></div></a>
 
-                <a href="https://www.linkedin.com/in/wilson-espina" target="blank"><div className="linkedin"><i className="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn - <span className="span-white"><em>https://www.linkedin.com/in/wilson-espina/</em></span></div></a>
+                <a href="https://www.linkedin.com/in/wilson-espina" target="blank"><div className="linkedin"><i className="fa fa-linkedin-square" aria-hidden="true"></i><span className="disappear">LinkedIn - </span><span className="span-white"><em>https://www.linkedin.com/in/wilson-espina/</em></span></div></a>
 
-                <a href="https://twitter.com/Wilson_Espina" target="blank"><div className="twitter"><i className="fa fa-twitter-square" aria-hidden="true"></i> Twitter - <span className="span-white"><em>Wilson_Espina</em></span></div></a>
+                <a href="https://twitter.com/Wilson_Espina" target="blank"><div className="twitter"><i className="fa fa-twitter-square" aria-hidden="true"></i><span className="disappear">Twitter - </span><span className="span-white"><em>Wilson_Espina</em></span></div></a>
 
                 {/* <div className="ga-profile"><img src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/portfolio/generalassembly_icon.png" className="ga-icon" /> GA Profile</div> */}
               </div>
